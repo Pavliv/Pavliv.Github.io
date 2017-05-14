9da349c1f8672aaeaf9b66bb695fc7ffe04b2100
Pavliv <maryan.pavliv@gmail.com>
2017-05-14 23:37:57 +0300

    angular app

---
+++ scripts/app.js
@@ -0,0 +1,41 @@
+angular.module('app', [])
+	.controller("mainCtrl", [function(){
+		var $ctrl = this;
+		$ctrl.index = 0;
+		$ctrl.data = localStorage.getItem("data")
+				? JSON.parse(localStorage.getItem("data"))
+				: [];
+		$ctrl.comments = $ctrl.data[0] && $ctrl.data[0].comments.length
+				? $ctrl.data[0].comments
+				: [];
+		$ctrl.addItem = function(el){
+			$ctrl.data.push({ value:el, comments:[] });
+			localStorage.setItem("data", JSON.stringify($ctrl.data));
+			$ctrl.itemValue = "";
+		};
+		$ctrl.rmItem = function(i){
+			$ctrl.data.splice(i, 1);
+			localStorage.setItem("data", JSON.stringify($ctrl.data));
+		};
+		$ctrl.showComments = function(i){
+			$ctrl.index = i;
+			$ctrl.comments = $ctrl.data[i].comments;
+		};
+		$ctrl.addComment = function(text){
+			$ctrl.data[$ctrl.index].comments.push(text);
+			localStorage.setItem("data", JSON.stringify($ctrl.data));
+			$ctrl.comment = "";
+		};
+	}])
+	.directive('ngEnter', function() {
+		return function(scope, element, attrs) {
+			element.bind("keydown", function(e) {
+				if(e.which === 13) {
+					scope.$apply(function(){
+						scope.$eval(attrs.ngEnter, {'e': e});
+					});
+					e.preventDefault();
+				}
+			});
+		};
+	});
---
+++ scripts/example.js
@@ -0,0 +1,32 @@
+[
+	{
+		value: "",
+		comments: [
+			{
+				userId:1,
+				photoUrl: '',
+				text: ''
+			},
+			{
+				userId:2,
+				photoUrl: '',
+				text: ''
+			}
+		]
+	},
+	{
+		value: "",
+		comments: [
+			{
+				userId:1,
+				photoUrl: '',
+				text: ''
+			},
+			{
+				userId:2,
+				photoUrl: '',
+				text: ''
+			}
+		]
+	}
+]
