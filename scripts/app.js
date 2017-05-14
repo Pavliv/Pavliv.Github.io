d013f4c6ce7ec0f4528c19571caf970e031400ad
Pavliv <maryan.pavliv@gmail.com>
2017-05-15 00:46:42 +0300

    fix some code

--- scripts/app.js
+++ scripts/app.js
@@ -5,22 +5,31 @@ angular.module('app', [])
 		$ctrl.data = localStorage.getItem("data")
 				? JSON.parse(localStorage.getItem("data"))
 				: [];
+
 		$ctrl.comments = $ctrl.data[0] && $ctrl.data[0].comments.length
 				? $ctrl.data[0].comments
 				: [];
+
 		$ctrl.addItem = function(el){
 			$ctrl.data.push({ value:el, comments:[] });
 			localStorage.setItem("data", JSON.stringify($ctrl.data));
 			$ctrl.itemValue = "";
+
+			if($ctrl.data.length == 1){
+				$ctrl.comments = $ctrl.data[0].comments;
+			};
 		};
+
 		$ctrl.rmItem = function(i){
 			$ctrl.data.splice(i, 1);
 			localStorage.setItem("data", JSON.stringify($ctrl.data));
 		};
+
 		$ctrl.showComments = function(i){
 			$ctrl.index = i;
 			$ctrl.comments = $ctrl.data[i].comments;
 		};
+
 		$ctrl.addComment = function(text){
 			$ctrl.data[$ctrl.index].comments.push(text);
 			localStorage.setItem("data", JSON.stringify($ctrl.data));
